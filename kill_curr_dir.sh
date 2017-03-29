1d1c835ac3cb69290a722e4882445590173c1513
Konstantin Kowalski <konstantin@businesscape.com>
2017-03-29 13:56:17 +0000

    modifying kill_current_gunni.sh

--- kill_curr_dir.sh
+++ kill_curr_dir.sh
@@ -6,7 +6,12 @@
 #http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x721.html
 
 
-sudo kill -9 `ps -e | grep guni | grep "pts/46" | awk '{ print $1 }' | tr '\n' ' '`
+###latest working: sudo kill -9 `ps -e | grep guni | grep "pts/46" | awk '{ print $1 }' | tr '\n' ' '`
 
 #kill pywatch
 #ps -e | grep pywatch | grep "pts/46" | awk '{ print $1 }' | tr '\n' ' '
+
+
+
+kill `lsof . | grep pywatch | awk '{ print $2}' | tr '\n' ' ' `
+kill `lsof . | grep gunicorn | awk '{ print $2}' | tr '\n' ' ' `
